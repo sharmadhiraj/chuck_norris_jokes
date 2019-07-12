e8ba0e485d19f7ce84e62230ea78b416828d4415
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2019-07-12 16:18:29 +0545

    Fetch & display joke.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_web/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() => runApp(App());
 
@@ -16,7 +18,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Chuck Norris Jokes")),
-      body: Center(child:Text("Chuck Norris Jokes")),
+      body: FutureBuilder<String>(
+        future: fetchJoke(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              return Center(
+                child: Text(snapshot.hasError ? snapshot.error : snapshot.data,
+                    style: TextStyle(fontSize: 22.0)),
+              );
+          }
+        },
+      ),
     );
+  }
+
+  Future<String> fetchJoke() async {
+    var url = "https://api.chucknorris.io/jokes/random?category=dev";
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      return jsonResponse['value'];
+    } else {
+      return ("Unexpected error occurred.");
+    }
   }
 }
